a66a9b1a74e1bbdaaf03eedc4208394872d6f239
Hurng-Chun Lee <h.lee@donders.ru.nl>
2017-01-03 20:13:38 +0100

    get redis host and port from environmental variables
    - REDIS_HOST
    - REDIS_PORT

--- docker/stager/stager.js
+++ docker/stager/stager.js
@@ -8,8 +8,8 @@ var child_process = require('child_process')
 var fs = require('fs')
 var queue = kue.createQueue({
     redis: {
-        port: 6379,
-        host: '127.0.0.1'
+        port: process.env.REDIS_PORT,
+        host: process.env.REDIS_HOST
     }
 });
 var path = require('path');
