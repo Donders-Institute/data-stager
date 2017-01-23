e19b6a10b31b65a52f02fae2fb6a2f3447c5317a
Hurng-Chun Lee <hclee.work@gmail.com>
2017-01-23 15:09:31 +0100

    update project2collection.js
    - query iCAT to get collection namespace of a given project

---
+++ docker/stager/bin/s-getcoll4project.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function print_usage() {
+
+    cat <<EOF
+Usage:
+
+  $ s-getcoll4project.sh <collOu> <collType> <projectId>
+
+EOF
+}
+
+# check if necessary arguments are presented
+if [ $# -ne 3 ]; then
+    print_usage
+    exit 1
+fi
+
+irule "uiFindCollection('organisationalUnit=${1}%type=${2}%projectId=${3}', false, *out)" "null" "*out" | awk -F ' = ' '{print $2}'
--- docker/stager/routes/project2collection.js
+++ docker/stager/routes/project2collection.js
@@ -1,4 +1,4 @@
-var auth = require('basic-auth');
+const child_process = require('child_process');
 
 var _getCollNameByProject = function(request, response) {
 
@@ -14,11 +14,41 @@ var _getCollNameByProject = function(request, response) {
     var collNameCatchall = (map['_CATCHALL']) ? map['_CATCHALL'] + '/' + projectId:undefined;
     var collName = (map[projectId]) ? map[projectId]:collNameCatchall;
 
+    // call the external script to get the collection name using irule
+    var errmsg = '';
+    if ( ! collName ) {
+        var abbrTypes = {
+            'DAC': 'DATA_ACQUISITION',
+            'RDC': 'RESEARCH_DOCUMENTATION',
+            'DSC': 'DATA_SHARING'
+        };
+
+        var cmd = __dirname + '/../bin/s-getcoll4project.sh';
+        var cmd_args = ['DCCN', abbrTypes[collType], projectId];
+        var cmd_opts = {
+            timeout: 60*1000,
+            maxBuffer: 10*1024*1024
+        };
+
+        try {
+            var data = JSON.parse(child_process.execFileSync(cmd, cmd_args, cmd_opts));
+            if ( data.collections.length > 1 ) {
+                throw new Error('more than one ' + collType + ' collections found for project ' + projectId);
+            } else if ( data.collections.length == 0 ) {
+                throw new Error(collType + ' collection not found for project ' + projectId);
+            } else {
+                collName = data.collections[0]['collName'];
+            }
+        } catch( err ){
+            errmsg = err.toString();
+        }
+    }
+
     if ( collName ) {
         response.json({'collName': collName});
     } else {
         response.status(404);
-        response.json({'errmsg': collType + ' not found for project: ' + projectId});
+        response.json({'errmsg': errmsg});
     }
 }
 
