0817172db730f3a9a019790d2ddb2796b616c3cb
Hurng-Chun Lee <hclee.work@gmail.com>
2017-10-31 11:15:09 +0100

    add html encoding and decoding functions

--- docker/stager-ui/public/javascripts/utility.js
+++ docker/stager-ui/public/javascripts/utility.js
@@ -1,23 +1,41 @@
 jQuery.fn.autoWidth = function(options) {
-  var settings = { 
-        limitWidth   : false 
-  } 
+  var settings = {
+        limitWidth   : false
+  }
 
-  if(options) { 
-        jQuery.extend(settings, options); 
-    }; 
+  if(options) {
+        jQuery.extend(settings, options);
+    };
 
-    var maxWidth = 0; 
+    var maxWidth = 0;
 
-  this.each(function(){ 
-        if ($(this).width() > maxWidth){ 
-          if(settings.limitWidth && maxWidth >= settings.limitWidth) { 
-            maxWidth = settings.limitWidth; 
-          } else { 
-            maxWidth = $(this).width(); 
-          } 
-        } 
-  });   
+  this.each(function(){
+        if ($(this).width() > maxWidth){
+          if(settings.limitWidth && maxWidth >= settings.limitWidth) {
+            maxWidth = settings.limitWidth;
+          } else {
+            maxWidth = $(this).width();
+          }
+        }
+  });
 
-  this.width(maxWidth); 
+  this.width(maxWidth);
+}
+
+htmlEncode = function(value) {
+    return value.replace(/&/g, '&amp;')
+                .replace(/"/g, '&quot;')
+                .replace(/'/g, '&#39;')
+                .replace(/</g, '&lt;')
+                .replace(/ /g, '&nbsp;')
+                .replace(/>/g, '&gt;');
+}
+
+htmlDecode = function(value) {
+    return value.replace(/&quot;/g, '"')
+                .replace(/&#39;/g, "'")
+                .replace(/&lt;/g, '<')
+                .replace(/&gt;/g, '>')
+                .replace(/&nbsp;/g, ' ')
+                .replace(/&amp;/g, '&');
 }
