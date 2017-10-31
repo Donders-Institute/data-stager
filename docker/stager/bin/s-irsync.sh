11335fc93236279f7692b3c231d6826f12c0104b
Hurng-Chun Lee <h.lee@donders.ru.nl>
2017-10-31 15:43:18 +0100

    apply the new irsync script for production

--- docker/stager/bin/s-irsync.sh
+++ docker/stager/bin/s-irsync.sh
@@ -5,7 +5,12 @@ function print_usage() {
     cat <<EOF
 Usage:
 
-  $ s-irsync.sh <src> <dst> <rdmUser> <rdmPass>
+  $ s-irsync.sh <srcPath> <dstPath> <irodsUserName> <irodsA>
+
+  - srcPath:       the source location, use "irods:" prefix to indicate it's an iRODS path
+  - dstPath:       the target location, use "irods:" prefix to indicate it's an iRODS path
+  - irodsUserName: the iRODS username 
+  - irodsA:        the path of the .irodsA file in which a scrambled password is stored
 
 EOF
 }
@@ -29,6 +34,15 @@ function get_script_dir() {
     echo "$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 }
 
+# wrapper function for parallel to call for file transfer
+function file_transfer() {
+    ${1} -f -K "${2}" "${3}" >/dev/null 2>&1
+    ec=$?
+    echo $4
+    return $ec
+}
+export -f file_transfer
+
 # check if control file is given
 if [ $# -ne 4 ]; then
     print_usage
@@ -46,9 +60,11 @@ export IRODS_AUTHENTICATION_FILE=$4
 w_total=0
 
 # check source type/existence
+is_src_irods=0
 is_src_dir=0
 echo $src | egrep '^i:' > /dev/null 2>&1
 if [ $? -eq 0 ]; then
+    is_src_irods=1
     src_coll=$( echo $src | sed 's/^i://' | sed 's/\/$//' )
     ils "$src_coll" > /dev/null 2>&1
     if [ $? -ne 0 ]; then
@@ -82,12 +98,12 @@ else
 fi
 
 ## prepare destination directory/collection
-is_irods=0
+is_dst_irods=0
 is_dst_dir=0
 
 echo $dst | egrep '^i:' > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    is_irods=1
+    is_dst_irods=1
     dst_coll=$( echo $dst | sed 's/^i://' )
     # TODO: find a better way to determine whether the irods namespace is existing and whether it's a directory
     ils "${dst_coll}" > /dev/null 2>&1
@@ -121,7 +137,7 @@ fi
 
 # make sure the dst_dir is created
 if [ $is_dst_dir -eq 1 ]; then
-    if [ $is_irods -eq 1 ]; then
+    if [ $is_dst_irods -eq 1 ]; then
         imkdir -p "$( echo $dst | sed 's/^i://' )" > /dev/null 2>&1
     else
         mkdir -p "${dst}"
@@ -136,34 +152,170 @@ fi
 
 # run irsync
 if [ $w_total -gt 0 ]; then
-    w_done=0
-    w_done_percent=0
-    ${mydir}/s-unbuffer irsync -v -K -r "${src}" "${dst}" | while read -r line; do
 
-        if [[ $line == *"ERROR:"* ]]; then
-            # return the whole line containing the ERROR: string
-            echo "error:${line}"
-        else
+    # transferring files
+    ec=0
+    nf_threshold=100000
+
+    # log files
+    flist=/tmp/files2sync_$$.txt
+    flog=/tmp/files2sync_$$.log
+
+    # make sure flog and flist are new
+    if [ -f $flog ]; then
+        rm -f $flog
+    fi
+        
+    if [ -f $flist ]; then
+        rm -f $flist
+    fi
+
+    # create empty flist
+    touch $flist
+
+    if [ $w_total -lt $nf_threshold ]; then
+        # small dataset with files less than 200,000
+        ${mydir}/s-unbuffer irsync -v -K -r "${src}" "${dst}" | while read -r line; do
+
+            if [[ $line == *"ERROR:"* ]]; then
+                # return the whole line containing the ERROR: string
+                echo "error:${line}"
+            else
+                w_done=$(( $w_done + 1 ))
+                w_done_percent=$(( $w_done * 100 / $w_total ))
+
+                # the process is still running, therefore the progress should not exceed 99%
+                if [ $w_done -ge $w_total ] || [ $w_done_percent -ge 100 ]; then
+                    w_done=$(( $w_total - 1 ))
+                    w_done_percent=99
+                fi
+
+                # print current progress
+                echo "progress:${w_done_percent}:${w_done}:${w_total}"
+            fi
+        done
+        # catch the exit code of the actual irsync command
+        ec=${PIPESTATUS[0]}
+    else
+        # more scalable approach for dataset containing massive amount of files
+        # scan files to be synchronised
+        isrc=$( echo $src | sed 's/^i://g' )
+
+        # increate the w_total by 10% to take into account the scanning overhead
+        # w_total=$( echo "($w_total + 0.1 * $w_total)/1" | bc )
+
+	${mydir}/s-unbuffer irsync -v -l -r "${src}" "${dst}" 2>/dev/null | grep -v '^C- ' | while read -r line; do
+            ## keep file to be transferred in the flist
+            do_cnt=0
+            echo $line | grep 'a match no sync required' >/dev/null 2>&1
+	    if [ $? -ne 0 ]; then
+                # keep only the line like, because we want the full path:
+                # example would be:
+                # 
+                # /var/lib/irods/test/filesystem/mapping.txt   522206   N
+                # 
+                echo $line | grep "^${isrc}" >/dev/null 2>&1
+                if [ $? -eq 0 ]; then
+                    do_cnt=1
+
+                    # make sure the line contains only the filename to be transferd
+                    # line example with "space" on file name:
+                    #
+                    # /opt/stager/dummy/test data.99 10240 N
+                    #
+                    line=$( echo $line | awk '{out=$1} { for (i=2; i<NF-1; i++) {out=out" "$i;} } {print out}' )
+                    echo "${line}" >> $flist
+                fi
+            else
+                do_cnt=1
+            fi
+
+            if [ $do_cnt -eq  1 ]; then
+                w_done=$(( $w_done + 1 ))
+                w_done_percent=$(( $w_done * 100 / $w_total ))
+                echo "progress:${w_done_percent}:${w_done}:${w_total}"
+                #w_done_f=$(( $w_done * 10 / $w_total ))
+                #w_done_percent=$(( $w_done_f * 100 / $w_total ))
+                #echo "progress:${w_done_percent}:${w_done_f}:${w_total}"
+            fi
+        done
+
+        # check status of the irsync scan
+        ec=${PIPESTATUS[0]}
+        if [ $ec -ne 0 ]; then
+            echo "error: irsync scan failed"
+            exit $ec
+        fi
+
+        # calculate the current progress as the $w_done is not available in this scope
+        w_done=$(( $w_total - $( cat $flist | wc -l ) ))
+
+        # remove the heading 'i:' indicating the iRODS endpoint
+        dst=$( echo $dst | sed 's/^i://g' )
+        src=$( echo $src | sed 's/^i://g' )
+
+        # create each sub-directories in the destination location
+        cat ${flist} | sed "s|${src}|${dst}|" | awk -F '/' 'BEGIN {OFS="/"} {$NF=""; print}' | sort | uniq | while read -r line; do
+            if [ $is_dst_irods -eq 1 ]; then
+                imkdir -p "$line" >> ${flog} 2>&1
+            else
+                mkdir -p "$line" >> ${flog} 2>&1
+            fi
+        done
+
+        # determin transfer method
+        cmd=""
+        if [ $(( $is_src_irods + $is_dst_irods )) -eq 2 ]; then
+            cmd='icp'
+        elif [ $is_src_irods -eq 1 ]; then
+            cmd='iget'
+        elif [ $is_dst_irods -eq 1 ]; then
+            cmd='iput'
+        fi
+
+        if [ "$cmd" == "" ]; then
+            echo "error: unable to determin the command to transfer"
+            exit 1
+        fi
+
+        # perform transfer with iput/iget, and parallelised by 'parallel'
+        # !! NOTE !!
+        # - the very complex awk is to create an additional line with "src" directory replaced properly by "dst"
+        # - the GNU parallel then takes the two lines as inputs to the "file_transfer" function exported from this script
+        # - we also assume one single file should not take more than 1800 seconds to transfer
+        # TODO: maybe useful to add "retry" feature ??
+        cat $flist | awk -v src="$src" -v dst="$dst" '{ print; $0 = substr($0,1,0) dst substr($0,1+length(src)); print }' | parallel --will-cite --timeout 1800 -N 2 -P 4 -k file_transfer ${cmd} "{1}" "{2}" "{#}" | while read -r line; do
+
             w_done=$(( $w_done + 1 ))
             w_done_percent=$(( $w_done * 100 / $w_total ))
-         
+
             # the process is still running, therefore the progress should not exceed 99%
             if [ $w_done -ge $w_total ] || [ $w_done_percent -ge 100 ]; then
                 w_done=$(( $w_total - 1 ))
                 w_done_percent=99
             fi
-         
+
             # print current progress
             echo "progress:${w_done_percent}:${w_done}:${w_total}"
-        fi
-    done
+        done
+  
+        ec=${PIPESTATUS[2]}
+    fi
 
-    # catch the exit code of the actual irsync command
-    ec=${PIPESTATUS[0]}
     # make sure the final 100% progress is printed
     if [ $ec -eq 0 ]; then
         echo "progress:100:${w_total}:${w_total}"
     fi
+
+    # cleanup flog and flist files
+    if [ -f $flog ]; then
+        rm -f $flog
+    fi
+        
+    if [ -f $flist ]; then
+        rm -f $flist
+    fi
+
     # return the irsync exit code
     exit $ec
 else
