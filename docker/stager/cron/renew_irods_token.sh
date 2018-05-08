4f184738c87cb395bf20c96098856f53cdab015b
Hurng-Chun Lee <h.lee@donders.ru.nl>
2018-05-08 17:15:27 +0200

    Update for being used in production
    - added docker-compose file for swarm cluster with docker stack
    - replaced IRODS_ADMIN_PASS with IRODS_ADMIN_CREDENTIAL, a file containing the iRODS admin's password
      * this enables the usage of the docker secrets.
    - copied the default crontab and irods token renewal script into the container.
    - updated the irods credential renewal script to make use of the IRODS_ADMIN_CREDENTIAL env. variable.

--- docker/stager/cron/renew_irods_token.sh
+++ docker/stager/cron/renew_irods_token.sh
@@ -2,4 +2,4 @@
 
 source /opt/stager/envvars
 
-$NODEJS_PREFIX/bin/node -e "var cfg=require('config'); var pass = cfg.get('RDM.userPass'); console.log(pass)" | iinit 
+cat $IRODS_ADMIN_CREDENTIAL | iinit
